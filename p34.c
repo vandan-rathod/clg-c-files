371ccbc3c4b2d50c09bdde1645f90627b5cb6c56
vandan-rathod <vandanrathod1551@gmail.com>
2025-08-26 22:55:43 +0530

    file added

---
+++ p34.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int i,n;
+    printf("Enter N=");
+    scanf("%d",&n);
+    for ( i = 1; i <=n; i++)
+    {
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
